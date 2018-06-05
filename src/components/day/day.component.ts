06494f2fb6412d2350c53c0cd05fab5323b09d52
Todor Pavlov <pavlovt@gmail.com>
2018-06-05 14:49:32 +0300

    show data in day view

--- src/components/day/day.component.ts
+++ src/components/day/day.component.ts
@@ -31,19 +31,21 @@ export class DayComponent implements OnInit {
 
   startToPx(jsdate) {
     const start = moment(jsdate).format('YYYY-MM-DD 00:00:00')
-    console.log(start, moment(jsdate).format('YYYY-MM-DD HH:mm:ss'), moment(jsdate).diff(moment(start)), parseInt(moment(jsdate).diff(moment(start))/1000/60*(this.conf.pxPerHour/60), 10))
+    // console.log(start, moment(jsdate).format('YYYY-MM-DD HH:mm:ss'), moment(jsdate).diff(moment(start)), parseInt(moment(jsdate).diff(moment(start))/1000/60*(this.conf.pxPerHour/60), 10))
     // return the diff in minutes
-    return parseInt(moment(jsdate).diff(moment(start))/1000/60*(this.conf.pxPerHour/60), 10);
+    const z: any = moment(jsdate).diff(moment(start))/1000/60*(this.conf.pxPerHour/60);
+    return parseInt(z, 10);
   }
 
   lenToPx(item) {
     let start = moment(item.startDate),
         end = moment(item.endDate);
-        console.log(start.format('HH:mm'), end.format('HH:mm'), moment(end).diff(moment(start)))
+        // console.log(start.format('HH:mm'), end.format('HH:mm'), moment(end).diff(moment(start)))
     // the event is longer than one day
     // then let its' height be until the end of today
     if (end.date() != start.date()) end = moment(moment(item.startDate).format('YYYY-MM-DD 23:59:59'))
     // return the diff in minutes
-    return parseInt(moment(end).diff(moment(start))/1000/60*(this.conf.pxPerHour/60), 10);
+    const z: any = moment(end).diff(moment(start))/1000/60*(this.conf.pxPerHour/60);
+    return parseInt(z, 10);
   }
 }
